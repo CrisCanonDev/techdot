eef6e3864f7cf0f6ad4e177ae28b384688fe799c
Luke <147688832+luuuuukez@users.noreply.github.com>
2025-05-10 12:59:43 +0300

    Update voting.sol
    - Preventing duplicate voting (one person, one vote)
    - Voting Deadline
    - Result view

--- voting.sol
+++ voting.sol
@@ -8,6 +8,7 @@ contract Voting {
   */
   
   mapping (bytes32 => uint8) public votesReceived;
+  mapping ()
   
   /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
   We will use an array of bytes32 instead to store the list of candidates
@@ -15,12 +16,22 @@ contract Voting {
   
   bytes32[] public candidateList;
 
+
+  // one person one vote
+  mapping (address => bool) public hasVoted;
+
+  //deadline 
+  uint public votingDeadline;
+
   /* This is the constructor which will be called once when you
   deploy the contract to the blockchain. When we deploy the contract,
   we will pass an array of candidates who will be contesting in the election
   */
-  function Voting(bytes32[] candidateNames) {
+  function Voting(bytes32[] candidateNames, uint durationInSeconds) {
     candidateList = candidateNames;
+
+    //deadline
+    votingDeadline = now+ durationInSeconds;
   }
 
   // This function returns the total votes a candidate has received so far
@@ -32,8 +43,16 @@ contract Voting {
   // This function increments the vote count for the specified candidate. This
   // is equivalent to casting a vote
   function voteForCandidate(bytes32 candidate) {
+
+    // check deadline & if voted
+    require(now < votingDeadline);
+    require(!hasVoted[msg.sender]);
+
     if (validCandidate(candidate) == false) throw;
     votesReceived[candidate] += 1;
+
+    //set voted
+    hasVoted[msg.sender] = true;
   }
 
   function validCandidate(bytes32 candidate) returns (bool) {
@@ -44,4 +63,22 @@ contract Voting {
     }
     return false;
   }
+
+  //get winner
+  function getWinner() public view returns (bytes32) {
+    require(now >= votingDeadline);
+    bytes32 winner;
+    uint8 highestVotes = 0;
+
+    for (uint i = 0; i < candidateList.length; i++) {
+      if (votesReceived[candidateList[i]] > highestVotes) {
+        highestVotes = votesReceived[candidateList[i]];
+        winner = candidateList[i];
+      }
+    }
+    return winner;
+  }
+
+
+
 }
